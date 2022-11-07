818692300d78a4cabb23687f8a5e07ec6eb9af59
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2022-11-07 10:09:05 -0300

    class 4

---
+++ src/A0004-type-objects/A0004-type-objects.ts
@@ -0,0 +1,16 @@
+const objetoA: {
+  chaveA: string;
+  readonly chaveB: string;
+  chaveC?: string;
+  [key: string]: unknown;
+} = {
+  chaveA: 'Valor A',
+  chaveB: 'Valor B',
+};
+
+objetoA.chaveA = 'Outro valor';
+objetoA.chaveB = 'Outro valor';
+objetoA.chaveC = 'Nova chave';
+objetoA.chaveD = 'Nova chave';
+
+console.log(objetoA);
