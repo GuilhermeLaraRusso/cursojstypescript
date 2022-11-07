645a4255ff0a14908bf684d1e0fe8b7cafd94511
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2022-11-07 11:15:46 -0300

    class 7

---
+++ src/A0007-type-null-and-undefined/A0007-type-null-and-undefined.ts
@@ -0,0 +1,24 @@
+let x;
+if (typeof x === 'undefined') x = 20;
+console.log(x * 2);
+
+export function createPerson(
+  firstName: string,
+  lastName?: string,
+): {
+  firstName: string;
+  lastName?: string;
+} {
+  return {
+    firstName,
+    lastName,
+  };
+}
+
+export function squareOf(x: unknown): number | null {
+  if (typeof x === 'number') return x * x;
+  return null;
+}
+
+const squareOfTwoNumber = squareOf(2);
+const squareOfTwoString = squareOf('2');
