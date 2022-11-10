b9e3458e468373b9eb00a6febdc4ce12b236bb87
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2022-11-10 09:38:19 -0300

    class 15

---
+++ src/A0015-functions/A0015-functions.ts
@@ -0,0 +1,18 @@
+type MapStringsCallback = (item: string) => string;
+
+function mapStrings(array: string[], callbackfn: MapStringsCallback): string[] {
+  const newArray: string[] = [];
+
+  for (let index = 0; index < array.length; index += 1) {
+    const item = array[index];
+    newArray.push(callbackfn(item));
+  }
+
+  return newArray;
+}
+
+const abc = ['a', 'b', 'c'];
+const abcMapped = mapStrings(abc, (item) => item.toUpperCase());
+
+console.log(abc);
+console.log(abcMapped);
