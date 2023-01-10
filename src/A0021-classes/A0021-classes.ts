870df497f1ce0428bb72087c96ec45598bcdbc61
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2023-01-10 16:23:23 -0300

    learning classes

---
+++ src/A0021-classes/A0021-classes.ts
@@ -0,0 +1,37 @@
+export class Empresa {
+  public readonly nome: string;
+  private readonly colaboradores: Colaborador[] = [];
+  protected readonly cnpj: string;
+
+  constructor(nome: string, cnpj: string) {
+    this.nome = nome;
+    this.cnpj = cnpj;
+  }
+
+  addColaborador(colaborador: Colaborador): void {
+    this.colaboradores.push(colaborador);
+  }
+
+  showColaboradores(): void {
+    for (const colaborador of this.colaboradores) {
+      console.log(colaborador);
+    }
+  }
+}
+
+export class Colaborador {
+  constructor(
+    public readonly nome: string,
+    public readonly sobrenome: string,
+  ) {}
+}
+
+const empresa1 = new Empresa('Facebook', '11.111.111/0001-11');
+const colaborador1 = new Colaborador('Luiz', 'Otávio');
+const colaborador2 = new Colaborador('Guilherme', 'Russo');
+const colaborador3 = new Colaborador('Thais', 'Kelly');
+empresa1.addColaborador(colaborador1);
+empresa1.addColaborador(colaborador2);
+empresa1.addColaborador(colaborador3);
+console.log(empresa1);
+empresa1.showColaboradores();
