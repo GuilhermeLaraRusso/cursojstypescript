8bac5af5dd36f379db19c3230e19a7d71d4d31a1
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2023-01-23 10:53:20 -0300

    polimorfic this

---
+++ src/A0040-this-polimorfico/A0040-this-polimorfico.ts
@@ -0,0 +1,62 @@
+export class Calculadora {
+  constructor(public numero: number) {}
+
+  add(n: number): this {
+    this.numero += n;
+    return this;
+  }
+
+  sub(n: number): this {
+    this.numero -= n;
+    return this;
+  }
+
+  div(n: number): this {
+    this.numero /= n;
+    return this;
+  }
+
+  mul(n: number): this {
+    this.numero *= n;
+    return this;
+  }
+}
+
+export class SubCalculadora extends Calculadora {
+  pow(n: number): this {
+    this.numero **= n;
+    return this;
+  }
+}
+
+const calculadora1 = new Calculadora(10);
+const calculadora2 = new SubCalculadora(10);
+
+calculadora1.add(5).mul(2).div(3).sub(4);
+console.log(calculadora1);
+
+calculadora2.add(3).mul(4).div(4).sub(1).pow(2);
+console.log(calculadora2);
+
+// Builder - estilo Gang of Four
+export class RequestBuilder {
+  private method: 'get' | 'post' | null = null;
+  private url: string | null = null;
+
+  setMethod(method: 'get' | 'post'): this {
+    this.method = method;
+    return this;
+  }
+
+  setUrl(url: string): this {
+    this.url = url;
+    return this;
+  }
+
+  send(): void {
+    console.log(`Enviando dados via ${this.method} para ${this.url}`);
+  }
+}
+
+const request = new RequestBuilder(); // Builder
+request.setUrl('http://www.google.com').setMethod('post').send();
